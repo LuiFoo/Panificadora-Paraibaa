375d1e11a81bf0a19fea4756b16c0eb329bf64cd
Luiz Guilherme Rótulo <luizgrbt@gmail.com>
2025-09-14 13:04:17 -0300

    Update index.tsx

--- src/components/Footer/ImagensSection/index.tsx
+++ src/components/Footer/ImagensSection/index.tsx
@@ -1,17 +1,6 @@
 import Image from "next/image";
 
 function ImagensSection() {
-    const images = [
-        { src:"/images/conjunto/image1.png", alt:"Logo"},
-        { src:"/images/conjunto/image2.png", alt:"Logo"},
-        { src:"/images/conjunto/image1.png", alt:"Logo"},
-        { src:"/images/conjunto/image2.png", alt:"Logo"},
-        { src:"/images/conjunto/image2.png", alt:"Logo"},
-        { src:"/images/conjunto/image1.png", alt:"Logo"},
-        { src:"/images/conjunto/image2.png", alt:"Logo"},
-        { src:"/images/conjunto/image1.png", alt:"Logo"},
-        
-    ]
 
     return(
           <section className=" mt-15 relative w-full h-120 max-[400px]:h-36 max-[500px]:h-45 max-[700px]:h-55 max-[1000px]:h-70 max-[1500px]:h-90"> 
