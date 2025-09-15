b87b4f6030db24ff6e2eb75f3f20901dc540f1b3
Luiz Guilherme Rótulo <luizgrbt@gmail.com>
2025-09-15 13:28:38 -0300

    Create page.tsx

---
+++ src/app/esqueci-senha/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default function EsqueciSenhaPage() {
+  return (
+    <>
+      <Header />
+
+      <main className="my-10 min-h-[70vh] flex flex-col items-center justify-center p-4 bg-white">
+        <div className="flex flex-col items-center justify-center gap-6 w-full max-w-md rounded-2xl shadow-xl p-10 bg-blue-200">
+          <h1 className="text-2xl font-bold text-center text-white">
+            Esqueci minha senha
+          </h1>
+
+          <p className="text-center text-lg text-white">
+            Redefinição de senha apenas no fábrica III 👍
+          </p>
+        </div>
+      </main>
+
+      <Footer showMap={false} />
+    </>
+  );
+}
